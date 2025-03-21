bc8b83adc334a746c2deccfd12cb13f36f18e3e9
komarovvyu <v.yu.komarov@gmai.com>
2025-03-21 16:16:20 +0700

    Add tests folder, Check matrix-vector stack product
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   tests/arrays.py
    		create_np_arrays():
    	  simple np.arrays generation
    		create_operate_matrix():
    	  np.matix - I, R, product operations
    	  np.matvec - matrix - vector stack product
    	  [:,i] - vector stack coordinates extraction

---
+++ tests/arrays.py
@@ -0,0 +1,52 @@
+"""
+Examples of creation, reshape, etc. of multidimensional np.arrays, lists etc.
+"""
+import numpy as np
+
+def create_np_arrays():
+    '''
+    Demonstration of np array creation routines
+    https://numpy.org/devdocs/reference/routines.array-creation.html
+    :return: None
+    '''
+
+    # numpy.zeros(shape, dtype=float, order='C', *, like=None)
+    # http://numpy.org/devdocs/reference/generated/numpy.zeros.html
+    a = np.zeros((2, 3)) # 2 rows (lines) of 3 columns
+    b = np.zeros_like(a)
+    c = np.zeros(a.shape)
+    print(a, b, c, sep="\n")
+    # t = np.zeros((2,), dtype=[('x', 'i4'), ('y', 'i4')])  ??? x, y ???
+
+def create_operate_matrix():
+    Rz_p90d = np.matrix('0. -1. 0.; 1. 0. 0.; 0. 0. 1.', dtype=np.float32) # 'line 1; line 2; line 3'
+    print(f"R = 90 degree rotation around z: \n {Rz_p90d}")
+    print(f"R^-1: \n {Rz_p90d.I};", "\n"
+          f"R^t: \n {Rz_p90d.T}") # Inverse, Transpose, As is, A1 - flattened as is, H - complex conjugate transpose
+    print(f"R*R: \n {Rz_p90d * Rz_p90d}", "\n"
+          f"R^2: \n {Rz_p90d ** 2}") # * and ** as matrix operations
+
+    col_vec = np.matrix('1.; 2.; 3.')
+    print(f"cv = column vector: \n {col_vec}", "\n",
+          f"R * cv: \n {Rz_p90d * col_vec}") # 3x3 (rotation) matrix - column vector multiplication
+    row_vec = np.matrix('1. 2. 3.')
+    print(f"rv = row vector: \n {row_vec}", "\n",
+          f"rv * R: \n {row_vec * Rz_p90d}") # column vector - 3x3 (rotation) matrix multiplication
+    print(f"rv * cv: \n {row_vec * col_vec}", "\n",
+          f"cv * rv: \n {col_vec * row_vec}") # results [[14.]] and 3x3 matrix
+
+    x = (1.0, 1.1, 1.2, 1.3)
+    y = (2.0, 2.1, 2.2, 2.3)
+    z = (3.0, 3.1, 3.2, 3.3)
+    print(f"vectors collection input: (x, y, z) = {(x, y, z)}")
+    vec_stack = np.array((x, y, z), dtype=np.float32).T # rows - x,y,z triplets
+    print(f"resulted stack of vectors: \n {vec_stack}", "\n",
+          f"i=1 (second) vector: \n {vec_stack[1]}")
+    print(f"R - vector stack product : \n {np.matvec(Rz_p90d, vec_stack)}")
+    x_rot, y_rot, z_rot = tuple(vec_stack[:, 0]), tuple(vec_stack[:, 1]), tuple(vec_stack[:, 2])
+    print(f"rotated vectors: (x, y, z) = \n",
+          f"({x_rot} ,\n {y_rot} ,\n {z_rot})")
+
+
+if __name__ == "__main__":
+    create_operate_matrix()
